c73862602714843b1855068d099d593963447796
farshad161 <farshad.tajedin@gmail.com>
2022-01-19 11:22:15 +0330

    removing class of component

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import reactDOM from "react-dom";
 import App from "./components/App";
 import "./App.css";
 
-reactDOM.render(<App className="app" />, document.querySelector("#root"));
+reactDOM.render(<App />, document.querySelector("#root"));
