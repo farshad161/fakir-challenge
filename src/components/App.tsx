7471b926bba17a9ed082b1a4b1db7f92b9a3225f
farshad161 <farshad.tajedin@gmail.com>
2022-01-19 09:41:22 +0330

    removingthe antd

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,12 @@
 import * as React from "react";
 import AutoComplete from "./AutoComplete";
-import { Layout } from "antd";
-
-const { Header, Footer, Content } = Layout;
 
 interface AppProps {
   className?: string;
 }
 class App extends React.Component<AppProps> {
   render(): React.ReactNode {
-    return (
-      <Layout>
-        <Header />
-        <Content>
-          <div className="site-layout-content">
-            <AutoComplete />
-          </div>
-        </Content>
-        <Footer />
-      </Layout>
-    );
+    return <AutoComplete />;
   }
 }
 
--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -1,29 +1,7 @@
 import React, { Component } from "react";
-import {
-  Divider,
-  Button,
-  Row,
-  Col,
-  AutoComplete as AutoCompletion,
-} from "antd";
-import { SearchOutlined } from "@ant-design/icons";
 class AutoComplete extends Component {
   render(): React.ReactNode {
-    return (
-      <div className="auto-complete">
-        <Divider orientation="left">Auto Complete</Divider>
-        <Row>
-          <Col span={12}>
-            <AutoCompletion size="large" placeholder="input here" />
-          </Col>
-          <Col span={12}>
-            <Button type="primary" size="large" icon={<SearchOutlined />}>
-              Search
-            </Button>
-          </Col>
-        </Row>
-      </div>
-    );
+    return <div>hello world....</div>;
   }
 }
 
