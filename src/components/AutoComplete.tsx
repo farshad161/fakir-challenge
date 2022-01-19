8ba78b5d8542aa2646f5cd5fcc0719b32fd2d6ca
farshad161 <farshad.tajedin@gmail.com>
2022-01-19 11:22:53 +0330

    adding elements

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import List from "./List";
+
 class AutoComplete extends Component {
   render(): React.ReactNode {
-    return <div>hello world....</div>;
+    const items: string[] = ["ali", "jafar"];
+    return (
+      <div className="autoComplete">
+        <input className="input-text" type="text" name="user" id="user" />
+        <input className="button info" type="button" value="Submit" />
+        {!!items.length ? <List items={items} /> : null}
+      </div>
+    );
   }
 }
 
